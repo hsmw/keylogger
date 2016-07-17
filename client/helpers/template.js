986ab2a3bd7d81a32c5afdc8b6f2286700f736dd
Beau Harrison <beau@fnal.gov>
2016-07-17 10:59:44 -0500

    Create Template.Header.onRender to assign .active to tab based on FlowRouter.path

--- client/helpers/template.js
+++ client/helpers/template.js
@@ -1,14 +1,19 @@
-Template.Header.helpers({
-    active: function() {
-        var route = FlowRouter.current().route.name;
-        console.log("route name: ",route);
-        var active = {
-            register: route == "register",
-            transactions: route == "transactions",
-            keys: route == "keys",
-            rules: route == "rules",
-            employees: route == "employees",
-        };
-        return active;
-    }
+Template.Header.onRendered(function() {
+    /*****************************************************************************/
+    /* This helper adds an active class to bootstrap navbar link that is the current page */
+    /*****************************************************************************/
+    var path = FlowRouter.current().route.path; //Router.current().location.get().path;
+    var paths = path.replace(/(\/[A-Za-z\-]+)/g, function(m, $1, idx, str) {
+        return str.slice(0, idx || str.length).replace(/\/$/, '') + ',';
+    })
+        .replace(/\,$/, '')
+        .split(',');
+
+    // find only links in bootstrap navbar
+    var $navbarAnchors = $('.navbar a');
+
+    paths.forEach(function(item) {
+        $navbarAnchors.parent("li").removeClass('active');
+        $navbarAnchors.removeClass('active').filter('[href="' + item + '"]').parent("li").addClass('active');
+    });
 });
