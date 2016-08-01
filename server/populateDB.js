77f50c0d276bf613fac311ae9ef5db99cf4d1b4c
Beau Harrison <beau@fnal.gov>
2016-08-01 16:19:54 -0500

    Working populate of keys, employees, and transactions

--- server/populateDB.js
+++ server/populateDB.js
@@ -1,21 +1,20 @@
 Meteor.startup(function(){
-    var faker = require('faker');
-
     function createKeys(count) {
         let keyCount = count || 10;
-        for (var i = 0; i < KeyCount; i++) {
+        for (var i = 0; i < keyCount; i++) {
             Keys.insert({
                 number: faker.random.number({min:1000, max:99999}),
+                name: faker.name.jobArea()
             });
         }
     }
 
     function createEmployees(count) {
         let empCount = count || 10;
-        for (var i = 0; i < EmpCount; i++) {
+        for (var i = 0; i < empCount; i++) {
             Employees.insert({
                 name: faker.name.findName(),
-                number: faker.random.number({min:100, max:50000}) + faker.random.arrayElement(["N","V","C"])
+                empId: faker.random.number({min:100, max:50000}) + faker.random.arrayElement(["N","V","C"])
             });
         }
     }
@@ -23,21 +22,32 @@ Meteor.startup(function(){
     function createTransactions(count) {
         let tranCount = count || 100;
 
-        // Get random key
-        var keyCount = Keys.count(),
-            randKeyIndex = faker.random.number({min:1,max:keyCount}),
-            randKey = Keys.find().skip(randIndex).limit(1);
-
-        // Get random employee
-        var empCount = Employees.count(),
-            randEmpIndex = faker.random.number({min:1,max:empCount}),
-            randEmp = Employees.find().skip(randIndex).limit(1);
-
         for (var i = 0; i < tranCount; i++) {
-            Transactions.insert({
-                key: randKey.number,
-                employee: randEmp.empId
-            });
+            // Get random key
+            var keyCount = Keys.find().count(),
+                randKeyIndex = faker.random.number({min:1,max:keyCount}),
+                randKey = Keys.find({},{skip:randKeyIndex, limit:1}).fetch();
+
+            // Get random employee
+            var empCount = Employees.find().count(),
+                randEmpIndex = faker.random.number({min:1,max:empCount}),
+                randEmp = Employees.find({},{skip:randEmpIndex, limit:1}).fetch();
+
+            console.log(randKey[0]);
+            console.log(randKey[0].number);
+            console.log(randEmp[0]);
+            console.log(randEmp[0].empId);
+
+            var keyInTran = Transactions.find({key:randKey[0].number}).fetch();
+
+            console.log(keyInTran);
+
+            if (!keyInTran) {
+                Transactions.insert({
+                    key: randKey[0].number,
+                    employee: randEmp[0].empId
+                });
+            }
         }
     }
 
