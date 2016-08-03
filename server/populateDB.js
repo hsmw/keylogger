4f0c84ea3cd39800e1689b9687e1ab5b9895f8d3
Beau Harrison <beau@fnal.gov>
2016-08-03 10:42:20 -0500

    Fix issues with populate race condition. Dumb implemnetation Transaction inserts.

--- server/populateDB.js
+++ server/populateDB.js
@@ -1,57 +1,47 @@
 Meteor.startup(function(){
-    function createKeys(count) {
-        let keyCount = count || 10;
-        for (var i = 0; i < keyCount; i++) {
-            Keys.insert({
-                number: faker.random.number({min:1000, max:99999}),
-                name: faker.name.jobArea()
-            });
+    Keys.remove({}, (err,numRm) => {
+        if (err) {
+            console.log("Error: ",err)
+        } else {
+            console.log("Keys removed: ",numRm)
         }
-    }
-
-    function createEmployees(count) {
-        let empCount = count || 10;
-        for (var i = 0; i < empCount; i++) {
-            Employees.insert({
-                name: faker.name.findName(),
-                empId: faker.random.number({min:100, max:50000}) + faker.random.arrayElement(["N","V","C"])
-            });
+    })
+    Employees.remove({}, (err,numRm) => {
+        if (err) {
+            console.log("Error: ",err)
+        } else {
+            console.log("Employees removed: ",numRm)
         }
-    }
-
-    function createTransactions(count) {
-        let tranCount = count || 100;
-
-        for (var i = 0; i < tranCount; i++) {
-            // Get random key
-            var keyCount = Keys.find().count(),
-                randKeyIndex = faker.random.number({min:1,max:keyCount}),
-                randKey = Keys.find({},{skip:randKeyIndex, limit:1}).fetch();
-
-            // Get random employee
-            var empCount = Employees.find().count(),
-                randEmpIndex = faker.random.number({min:1,max:empCount}),
-                randEmp = Employees.find({},{skip:randEmpIndex, limit:1}).fetch();
-
-            console.log(randKey[0]);
-            console.log(randKey[0].number);
-            console.log(randEmp[0]);
-            console.log(randEmp[0].empId);
-
-            var keyInTran = Transactions.find({key:randKey[0].number}).fetch();
-
-            console.log(keyInTran);
-
-            if (!keyInTran) {
-                Transactions.insert({
-                    key: randKey[0].number,
-                    employee: randEmp[0].empId
-                });
-            }
+    });
+    Transactions.remove({}, (err,numRm) => {
+        if (err) {
+            console.log("Error: ",err)
+        } else {
+            console.log("Transactions removed: ",numRm)
         }
-    }
+    });
 
-    createKeys();
-    createEmployees();
-    createTransactions();
+    let count = 10
+
+    for (var i = 0; i < count; i++) {
+        let keyNumber = faker.random.number({min:1000, max:99999}),
+            keyName = faker.name.jobArea(),
+            empName = faker.name.findName(),
+            empId = faker.random.number({min:100, max:50000}) + faker.random.arrayElement(["N","V","C"])
+
+        Keys.insert({
+            number: keyNumber,
+            name: keyName
+        })
+
+        Employees.insert({
+            name: empName,
+            empId: empId
+        })
+
+        Transactions.insert({
+            key: keyNumber,
+            employee: empId
+        })
+    }
 });
