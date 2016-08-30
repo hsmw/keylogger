6533e99ef9498e700182e94b01b79122eca958d7
Beau Harrison <beau@fnal.gov>
2016-08-30 09:26:54 -0500

    Training and Employees is basically done. There will likely be refinements in the future.

--- client/templates/public/employee/employee.js
+++ client/templates/public/employee/employee.js
@@ -22,14 +22,13 @@ Template.employee.helpers({
                         }
                     },{
                         fields:{
+                            title:1,
                             code:1,
                             type:1
                         }
                     }).map(function(obj) {
-                        return {
-                            href:"http://www-esh.fnal.gov/pls/default/schedule.show_course_details?this_fermi_id=0&this_course_code="+obj.code+"&this_instr_type="+obj.type,
-                            text:obj.code
-                        }
+                        obj.href = "http://www-esh.fnal.gov/pls/default/schedule.show_course_details?this_fermi_id=0&this_course_code="+obj.code+"&this_instr_type="+obj.type
+                        return obj
                     })
 
         return result
